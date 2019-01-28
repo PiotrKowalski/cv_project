f5fe972286f5d63c7867e1a98e93b2417faa10de
Piotr Kowalski <piotreku9@o2.pl>
2019-01-28 16:12:24 +0100

    Procfile Heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 import psycopg2
 import django_heroku
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
